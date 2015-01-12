d1794254977e48ce43ca64f6cbf956e1d86c356e
Mary-Jane Smyth <maryjane.smyth@abiquo.com>
2015-01-12 18:41:58 +0100

    working a bit better

--- update_confluence_pages.py
+++ update_confluence_pages.py
@@ -15,11 +15,13 @@ import os
 import logging
 from distutils.util import strtobool
 import collections
+import yaml
 
 class wikiAuth:
-	def __init__(self,auser,apassword):
+	def __init__(self,auser,apassword,atoken):
 		self.user=auser
 		self.password=apassword
+		self.token=atoken
 
 
 class wikiLoc:
@@ -28,17 +30,6 @@ class wikiLoc:
 		self.spaceKey=aspaceKey
 		self.parentTitle=aparentTitle
 
-class wikiProp:
-	def __init__(self,arw,aup,aex,amo,aal,adu,acu,aiv,ane):
-		self.rewriteAll = arw
-		self.updateAll = aup
-		self.existing = aex
-		self.modifier = amo
-		self.alternative = aal
-		self.duplicate = adu
-		self.custom = acu
-		self.invalid = aiv
-		self.new = ane
 
 def tree(): return collections.defaultdict(tree)
 
@@ -65,33 +56,36 @@ def get_properties_file():
 	user = properties['user']
 	password = properties['password']
 	wloc = wikiLoc(wikiUrl,spaceKey,parentTitle)
-	server = xmlrpclib.ServerProxy(wikiUrl + '/rpc/xmlrpc')
 
-	wauth = wikiAuth(user,password)
+	server = xmlrpclib.ServerProxy(wikiUrl + '/rpc/xmlrpc')
+	token = server.confluence2.login(user, password)
+	wauth = wikiAuth(user,password,token)
+
 	subdir = properties['subdir']
 
+	propd = {}
 	grw = properties['rewriteAll']
-	rw = proc_strbool(grw)
+	propd['rewriteAll'] = proc_strbool(grw)
 	gup = properties['updateAll']
-	up = proc_strbool(gup)
+	propd['updateAll'] = proc_strbool(gup)
 	gex = properties['existing']
-	ex = proc_strbool(gex)	
+	propd['original'] = proc_strbool(gex)	
 	gmo = properties['modifier']
-	mo = proc_strbool(gmo)
+	propd['modifier'] = proc_strbool(gmo)
 	gal = properties['alternative']
-	al = proc_strbool(gal)
+	propd['alternative'] = proc_strbool(gal)
 	gdu = properties['duplicate']
-	du = proc_strbool(gdu)
+	propd['duplicate'] = proc_strbool(gdu)
 	gcu = properties['custom']
-	cu = proc_strbool(gcu)
+	propd['custom'] = proc_strbool(gcu)
 	giv = properties['invalid']
-	iv = proc_strbool(giv)
+	propd['invalid'] = proc_strbool(giv)
 	gne = properties['new']
-	ne = proc_strbool(gne)
+	propd['new'] = proc_strbool(gne)
 
-	wprop = wikiProp(rw,up,ex,mo,al,du,cu,iv,ne)
+	
 
-	return (wloc,wauth,server,subdir,wprop)
+	return (wloc,wauth,server,subdir,propd)
 
 def get_updates_file(work_file):
 	# Load a json file 
@@ -99,13 +93,13 @@ def get_updates_file(work_file):
 	with open(work_file) as wfile:
 		wk_file = wfile.read().replace('\n', '')	
 		wk_file = wk_file.replace('\t', " ")
-		file_dict = json.loads(wk_file)
+		file_dict = yaml.load(wk_file)
 		logging.info("Work_file: %s " % work_file)
 		for ix, ick in enumerate(file_dict):
 			logging.info("%d: %s - %s" % (ix,ick,file_dict[ick]))
 	return file_dict
 
-def get_page(wikAuth,wikLoc,pageTitle,server,etoken):
+def get_page(wikAuth,wikLoc,pageTitle,server):
 # returns the page
 	page = {}
 	token = server.confluence2.login(wikAuth.user, wikAuth.password)	
@@ -118,14 +112,15 @@ def get_page(wikAuth,wikLoc,pageTitle,server,etoken):
 	return page		
 	
 
-def create_update_page(wikAuth,wikLoc,pagtitle,ncontent,server,parentId,xtoken):
+def create_update_page(wikAuth,wikLoc,pagtitle,ncontent,server,parentId):
+	print ("In create_update_page for %s :" % pagtitle)
 	# create or update pages as appropriate
-	token = server.confluence2.login(wikAuth.user, wikAuth.password)
+#	token = server.confluence2.login(wikAuth.user, wikAuth.password)
 	logging.info("Trying to update page %s " % pagtitle)
 	alt_filename = ""
-	gotpage = get_page(wikAuth,wikLoc,pagtitle,server,token)
-
+	gotpage = get_page(wikAuth,wikLoc,pagtitle,server)
 	if gotpage:
+		print ("Found existing page")
 		logging.info("Found existing page %s " % pagtitle)
 		# check if the page in Confluence is the 0001 file - and if not, get the file for the different page
 		pgcontent = gotpage['content']
@@ -136,10 +131,10 @@ def create_update_page(wikAuth,wikLoc,pagtitle,ncontent,server,parentId,xtoken):
 		logging.info("Overwrite of page %s " % pagtitle)
 		#  	print "Forced overwrite of page %s, modified by %s" % (modifier,pagetitle)
 		gotpage['content'] = ncontent
-#			token = server.confluence2.login(wikAuth.user, wikAuth.password)
+		token = server.confluence2.login(wikAuth.user, wikAuth.password)
 		server.confluence2.storePage(token, gotpage) 			   	
    	else:
-#		print "Creating new page %s" % (pagetitle)
+		print "Creating new page %s" % (pagtitle)
 		logging.info ("Trying to create new page %s" % (pagtitle))
 		newpage = {}
 	#	print "parentId: %s" % (parentId)
@@ -148,7 +143,7 @@ def create_update_page(wikAuth,wikLoc,pagtitle,ncontent,server,parentId,xtoken):
 		newpage['title'] = pagtitle
 		newpage['content'] = ncontent
 
-#		token = server.confluence2.login(wikAuth.user,wikAuth.password)
+		token = server.confluence2.login(wikAuth.user,wikAuth.password)
 		server.confluence2.storePage(token, newpage)
    		# create a new page
 
@@ -171,11 +166,12 @@ def open_content_file(subdir,content_file_page,content_file_name):
 def main():
 	logging.basicConfig(filename='update_pages.log',level=logging.DEBUG)
 	# load properties file with wiki properties
+	prop = {}
 	(loc,auth,cserver,subdir,prop) = get_properties_file()
-	itoken = cserver.confluence2.login(auth.user, auth.password)
+
 	# retrieve the parent page where the pages will be stored and get its ID
 	valid_rest = ['GET','DELETE','OPTIONS','PUT','POST']
-	parentPage = get_page(auth,loc,loc.parentTitle,cserver,itoken)
+	parentPage = get_page(auth,loc,loc.parentTitle,cserver)
 
 	if parentPage:
 	# I don't know how to handle this exception properly 
@@ -187,7 +183,7 @@ def main():
 		not_files = get_updates_file("wiki_prohibited.json.txt")
 		nup_files = get_updates_file("wiki_nup.json.txt")
 
-		if prop.rewriteAll:
+		if prop['rewriteAll'] == True:
 #			overwrite all pages
 #			don't look at updates file
 			for pagen in all_files:
@@ -196,109 +192,73 @@ def main():
 				# skip license and other prohibited files
 					logging.info("Skipping page - %s " % pagen)
 				else:
-					page_rest = split(pagen,"_") 
+					page_rest = pagen.split("_") 
 					if page_rest:
 						if page_rest[0] not in valid_rest:
 							logging.info("File name is not valid rest - %s " % pagen)
 						else:	
-							filecontent = open_content_file(subdir,pagen,all_files[pagen])
-							if filecontent:	
-								try:
-									create_update_page(auth,loc,pagen,newcontent,cserver,parentId,itoken)
-								except:
-									logging.info("Could not create or update page - %s " % pagen)
-		else:							
-			if prop.updateAll:
-	#			read updates file
-	#			update all pages using the default options
-				for pagen in all_files:
-				# pagen is the page name
-					if pagen in not_files:
+							cf = all_files[pagen]
+							cfp = os.path.join(subdir,cf)
+							ncf = open(cfp,'r')	
+							newcontent = ncf.read()
+							# create or update pages
+							try:
+								create_update_page(auth,loc,pagen,newcontent,cserver,parentId)
+							except:
+								logging.warning ("Could not update page - %s " % pagen)
+							ncf.close()
+		elif prop['updateAll'] == True:
+#			read updates file
+#			update all pages using the default options
+			for option in nup_files:
+				for pgnup in nup_files[option]:	
+					filenup = nup_files[option][pgnup]
+					if pgnup in not_files:
 					# skip license and other prohibited files
 						logging.info("Skipping page - %s " % pagen)	
 					else:
-						page_rest = split(pagen,"_") 
+						page_rest = pgnup.split("_") 
 						if page_rest:
 							if page_rest[0] not in valid_rest:
-								logging.info("File name is not valid rest - %s " % pagen)
-							else:	
-								filen = ""
-								fileupdate = upd_files[pagen]
-								invalid = re.match(fileupdate,r'invalid')
-								new = re.match(fileupdate,r'new')
-								if invalid:
-									logging.info("File name is invalid")
-								elif new:
-									filen = fileupdate	
-								else:	
-									irreg = re.sub(fileupdate,'custom\: |alternative\: |duplicate: ','')
-#									alternative = re.sub(fileupdate,r'alternative: ')
-#									duplicate = re.sub(fileupdate,r'duplicate: ')
-#									if custom or alternative or duplicate:
-									if irreg:
-										filen = irreg
-									filecontent = open_content_file(subdir,pagen,filen)
-									if filecontent:	
-										try:
-											create_update_page(auth,loc,pagen,newcontent,cserver,parentId,itoken)
-										except:
-											logging.info("Couldn't create or update page - %s " % pagen)
-									else:
-										logging.info("No file content - %s " % pagen)		
+								logging.info("File name is not valid rest - %s " % pgnup)
+							else:
+								logging.info("Trying to open file %s" % filenup)	
+								filecontent = open_content_file(subdir,pgnup,filenup)
+								if filecontent:	
+									try:
+										logging.info("Trying to create page %s " % pgnup)
+										create_update_page(auth,loc,pgnup,filecontent,cserver,parentId)
+									except:
+										logging.info("Cannot create or update page - %s - %s " % (pgnup,filenup))
+								else:
+									logging.info("No file content - %s " % pagnup)			
 
-			else:
+		else:
 #			read updates file
 #			update according to the options set by user
-				for pagen in all_files:
-				# pagen is the page name
-					if pagen in not_files:
-					# skip license and other prohibited files
-						logging.info("Skipping page - %s " % pagen)	
-					else:
-						page_rest = pagen.split("_") 
-						if page_rest:
-							if page_rest[0] not in valid_rest:
-								logging.info("File name is not valid rest - %s " % pagen)
-							else:
-								filen = ""	
-								fileupdate = upd_files[pagen]
-								if fileupdate:	
-									filen = ""
-									irreg = re.match(fileupdate,'(custom\: |alternative\: |duplicate\: |new\: |invalid: )([\w,\.]+)')
-									if irreg:
-										logging.info("Irreg %s " % irreg)
-										update_type = irreg.group(1)
-										update_file = irreg.group(2)
-									# custom = re.sub(fileupdate,r'custom: ','')
-									# alternative = re.sub(fileupdate,r'alternative: ','')
-									# invalid = re.match(fileupdate,r'invalid')
-									# new = re.sub(fileupdate,r'new: ','')	
-									# duplicate = re.sub(fileupdate,r'duplicate: ','')
-										if "custom" in update_type:
-											if prop.custom:
-												filen = update_file
-										if "alternative" in update_type:
-											if prop.alternative:
-												filen = update_file
-										if "invalid" in update_type:
-											if prop.invalid:
-												filen = all_files[pagen]	
-										if "new" in update_type:
-											if prop.new:
-												filen = update_file
-									else:
-										logging.info("None of the irregular options apply %s " % fileupdate)
-										filen = fileupdate			
-									logging.info("Trying to open file %s" % filen)	
-									filecontent = open_content_file(subdir,pagen,filen)
+			for option in nup_files:
+				for pgnup in nup_files[option]:	
+					if prop[option] == True:
+						filenup = nup_files[option][pgnup]
+						if pgnup in not_files:
+						# skip license and other prohibited files
+							logging.info("Skipping page - %s " % pagen)	
+						else:
+							page_rest = pgnup.split("_") 
+							if page_rest:
+								if page_rest[0] not in valid_rest:
+									logging.info("File name is not valid rest - %s " % pgnup)
+								else:
+									logging.info("Trying to open file %s" % filenup)	
+									filecontent = open_content_file(subdir,pgnup,filenup)
 									if filecontent:	
 										try:
-											logging.info("Trying to create page %s " % pagen)
-											create_update_page(auth,loc,pagen,newcontent,cserver,parentId,itoken)
+											logging.info("Trying to create page %s " % pgnup)
+											create_update_page(auth,loc,pgnup,filecontent,cserver,parentId)
 										except:
-											logging.info("Cannot create or update page - %s - %s " % (pagen,filen))
+											logging.info("Cannot create or update page - %s - %s " % (pgnup,filenup))
 									else:
-										logging.info("No file content - %s " % pagen)				
+										logging.info("No file content - %s " % pagnup)				
 
 	else:
 		logging.info("Can't find the parent page %s" % loc.parentTitle)			
